3e8c7e8e44a3788f69a6f408c93b845e63143eda
fasilmsageer <fasilmsageer@gmail.com>
2022-12-08 16:35:17 +0530

    addstudent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddstudentComponent } from './addstudent/addstudent.component';
+import { ViewstudentComponent } from './viewstudent/viewstudent.component';
+import { SearchstudentComponent } from './searchstudent/searchstudent.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoute=[
+  {
+    path:"",
+    component:AddstudentComponent
+  },
+  {
+    path:"view",
+    component:ViewstudentComponent
+  },
+  {
+    path:"search",
+    component:SearchstudentComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddstudentComponent,
+    ViewstudentComponent,
+    SearchstudentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/searchstudent/searchstudent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchstudent',
+  templateUrl: './searchstudent.component.html',
+  styleUrls: ['./searchstudent.component.css']
+})
+export class SearchstudentComponent {
+
+}
